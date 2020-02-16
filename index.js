f7b134290d325ffb6a6f4efd9f20728ebff9a761
Nyawira <albinamuturi@gmail.com>
2020-02-16 16:04:53 +0300

    add changes made to index.js and index.html files

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-<console.log('Hello World!');
+<console.log('Your Akan Name');
 
 function formdata() {
 
-  const maleNames = ['James', 'Jack', 'Mark', 'Alex', 'Donald'];
-  const femaleNames = ['Mary', 'Victoria', 'Jane', 'Esther', 'Faith'];
+  const daysOfTheWeeks= ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday', 'Saturday']
+  const maleNames = ['Kwasi', 'Kwadwo', 'kwabena', 'Yaw', 'Kofi','Kwame'];
+  const femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua','Ama'];
 
   // get the date from html
-  const dateOfBirth = document.getElementById('date-of-birth').value;
+  const dateOfBirth = parseInt(document.getElementById('date-of-birth')).value;
 
   const isMale = document.getElementById('male').checked;
   const isFemale = document.getElementById('female').checked;
@@ -15,14 +16,34 @@ function formdata() {
 
   // convert it to a date object
   const date = new Date(dateOfBirth);
-
-  // manipulate date object and get day here
-  const day = 'Monday';
+  var date = new Date(year +"/" + month + "/"+day);
+    var dayBorn = date.getDateOfBirth();
+    var akanName;
+    var validate = (year > 0 && (month >0 && month <=12) && (day > 0 && day <= 31));
+    var validateGender = (gender !== "male" && gender !== "female");
+    if (year <= 1800 ) {
+        alert("Invalid year");
+    }
+    else if (month <= 0 || month > 12) {
+     alert("Month is between 1 and 12");
+    }
+     else if (day <= 0 || day > 31){
+         alert("Date is between 1 and 31");
+    }
+     else if (validate == false) {
+         alert("Invalid Input!!");
+    }
 
   // randomly select male name
-  if (isMale) {
-    name = maleNames[Math.floor(Math.random() * maleNames.length)]
-  }
+  if(gender ==="male" && year > 0 && month > 0 && month < 13 && day > 0 && day < 32){
+        akanName = maleNames[dayBorn];
+    alert("You were born on "+dayOfTheWeek[dayBorn]+ " and your Akan name is "+akanName);
+    }
+    else if (gender ==="female" && year > 0 && month > 0 && month < 13 && day > 0 && day < 32){
+        akanName = femaleNames[dayBorn];
+    alert("You were born on "+dayOfTheWeek[dayBorn]+ " and your Akan name is "+akanName);
+    }
+
 
   // randomly select female name
   if (isFemale) {
