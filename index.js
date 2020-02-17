b98b8193f172a1c1e76791c278b1778acaf49a4d
Nyawira <albinamuturi@gmail.com>
2020-02-17 05:09:15 +0300

    change date method, input features of date method in index.js

--- index.js
+++ index.js
@@ -4,8 +4,31 @@
   const femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua','Ama'];
 
   // Form Validation
-  
-  const dateOfBirth = parseInt(document.getElementById('date-of-birth')).value;
-
+  function validate(){
+  const bdate = parseInt(document.getElementById('bdate')).value;
+  const bmonth = parseInt(document.getElementById('bmonth')).value;
+  const byear = parseInt(document.getElementById('byear')).value;
   const isMale = document.getElementById('male').checked;
   const isFemale = document.getElementById('female').checked;
+  var formValid= false;
+  var i=0;
+
+  if (bdate.value=="" || bdate.value == null) {
+    // alert (date required)
+    document.getElementById("dob").innerHTML="Input date";
+    return false;
+  } else {
+    if (!isNAN(bdate.Value)) {
+      if (bdate.value <=0 || bdate.value>31){
+        document.getElementById("dob").innerHTML = "Enter valid date";
+        return false;
+      } else{
+        document.getElementById('dob').innerHTML= 'proceed';
+      }
+    } else {
+      document.getElementById('dob').innerHTML = 'Enter a Number'
+      return false;
+    }
+  }
+  if ()
+}
